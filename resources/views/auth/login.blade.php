5a0e461bf4a513ee41dff56c1f39da8af225a180
lolzxc <guitarbeginner2@gmail.com>
2023-05-04 04:51:25 +0800

    updated sign in and login ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,51 +1,17 @@
 <x-guest-layout>
-    <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    {{-- <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
+    @endif
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form> --}}
-
-    <section class="h-100 gradient-form" style="background-color: #eee;">
+    {{-- <section class="h-100 gradient-form" style="background-color: #eee;">
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-xl-10">
@@ -103,68 +69,61 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> --}}
 
-</x-guest-layout>
-
-{{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-<section class="h-100 gradient-form" style="background-color: #eee;">
-    <div class="container py-5 h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-xl-10">
-          <div class="card rounded-3 text-black">
-            <div class="row g-0">
-              <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                  <h4 class="mb-4">We are more than just a company</h4>
-                  <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-              </div>
-              <div class="col-lg-6">
-                <div class="card-body p-md-5 mx-md-4">
-
-                  <div class="text-center mb-5">
-                    <p>Please login to your account</p>
+    <section class="h-100 gradient-form">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-xl-10">
+            <div class="card rounded-3" style="background-color: #e4d9ff;">
+              <div class="row g-0">
+                <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                  <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                    <h4 class="mb-4" style="font-family:Pixel;">We are more than just a company</h4>
+                    <p class="small mb-0">Pen & Pixel features a user-friendly interface where gamers can easily create and publish blog posts. You can connect with others and share your passion for gaming. Whether you're a casual gamer or a hardcore enthusiast, you're sure to find something of interest on Pen & Pixel!</p>
                   </div>
-
-                  <form>
-
-                    <div class="form-outline mb-4">
-                      <input type="email" id="form2Example11" class="form-control"
-                        placeholder="juandelacruz@example.com" />
-                      <label class="form-label" for="form2Example11">Email</label>
+                </div>
+                <div class="col-lg-6">
+                  <div class="card-body p-md-5 mx-md-4" >
+                    <div class="text-center mb-5" style="font-family:Pixel;">
+                      <p>Please login to your account</p>
                     </div>
-
-                    <div class="form-outline mb-4">
-                      <input type="password" id="form2Example22" class="form-control" placeholder="************" />
-                      <label class="form-label" for="form2Example22">Password</label>
-                    </div>
-
-                    <div class="text-center pt-1 mb-5 pb-1">
-                      <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button"
-                        style="width:100%">Login</button>
-                      <a class="text-muted" href="#!">Forgot password?</a>
-                    </div>
-
-                    <div class="d-flex align-items-center justify-content-center pb-4">
-                      <p class="mb-0 me-2">Don't have an account?</p>
-                      <a style="text-decoration: none;" href="registration.html">Create New.</a>
-                    </div>
-
-                  </form>
-
+  
+                    <form method="POST" action="{{ route('login')}}" enctype="multipart/form-data">
+                      @csrf
+  
+                      <div class="form-outline mb-4">
+                        <input type="email" id="form2Example11" class="form-control" name="email" required
+                          placeholder="juandelacruz@example.com" />
+                        <label class="form-label" for="form2Example11" style="font-family:Pixel;">Email</label>
+                      </div>
+  
+                      <div class="form-outline mb-4">
+                        <input type="password" id="form2Example22" class="form-control" placeholder="************" name="password" required/>
+                        <label class="form-label" for="form2Example22" style="font-family:Pixel;">Password</label>
+                      </div>
+  
+                      <div class="text-center pt-1 mb-5 pb-1" style="font-family:Pixel;">
+                        <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit"
+                          style="width:100%">Login</button>
+                        <a class="text-muted" href="#!">Forgot password?</a>
+                      </div>
+  
+                      <div class="d-flex align-items-center justify-content-center pb-4" style="font-family:Pixel;">
+                        <p class="mb-0 me-2" ;>Don't have an account?</p>
+                        <a style="text-decoration: none; font-size: 13px;" href="registration.html">Create New.</a>
+                      </div>
+  
+                    </form>
+  
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-    crossorigin="anonymous"></script> --}}
+</x-guest-layout>
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,67 +1,5 @@
 <x-guest-layout>
-    {{-- <form class="h-100" method="POST" action="{{ route('register') }}">
-				@csrf --}}
-
-    <!-- Name -->
-    {{-- <div>
-						<x-input-label for="name" :value="__('Name')" />
-						<x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-						<x-input-error :messages="$errors->get('name')" class="mt-2" />
-				</div>
-
-				<!-- Email Address -->
-				<div class="mt-4">
-						<x-input-label for="email" :value="__('Email')" />
-						<x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-						<x-input-error :messages="$errors->get('email')" class="mt-2" />
-				</div>
-
-				<!-- Password -->
-				<div class="mt-4">
-						<x-input-label for="password" :value="__('Password')" />
-
-						<x-text-input id="password" class="block mt-1 w-full"
-														type="password"
-														name="password"
-														required autocomplete="new-password" />
-
-						<x-input-error :messages="$errors->get('password')" class="mt-2" />
-				</div>
-
-				<!-- Confirm Password -->
-				<div class="mt-4">
-						<x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-						<x-text-input id="password_confirmation" class="block mt-1 w-full"
-														type="password"
-														name="password_confirmation" required autocomplete="new-password" />
-
-						<x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-				</div>
-
-				<!-- Upload Image -->
-				<div class="mt-4">
-						<x-input-label for="upload_image" :value="__('Upload Image')" />
-
-						<x-text-input id="upload_image" class="block mt-1 w-full"
-														type="file"
-														name="upload_image" required />
-
-						<x-input-error :messages="$errors->get('upload_image')" class="mt-2" />
-				</div>
-
-				<div class="flex items-center justify-end mt-4">
-						<a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-								{{ __('Already registered?') }}
-						</a>
-
-						<x-primary-button class="ml-4">
-								{{ __('Register') }}
-						</x-primary-button>
-				</div>
-		</form> --}}
-
-    <section class="h-100 gradient-form" style="background-color: #eee;">
+    {{-- <section class="h-100 gradient-form" style="background-color: #eee;">
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-xl-10">
@@ -119,8 +57,8 @@
                                         </div>
 
                                         <div class="form-outline mb-3">
-                                            <input type="password" id="form2Example22" name="password_confirmation" required
-                                                autofocus class="form-control form-control-lg"
+                                            <input type="password" id="form2Example22" name="password_confirmation"
+                                                required autofocus class="form-control form-control-lg"
                                                 placeholder="************" />
                                             <label class="form-label" for="form2Example22">Confirm Password</label>
                                         </div>
@@ -152,7 +90,7 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> --}}
 
     @if ($errors->any())
         <div class="alert alert-danger">
@@ -163,4 +101,90 @@
             </ul>
         </div>
     @endif
+
+    <section class="h-100 pt-5">
+      <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center">
+          <div class="col-xl-10 mb-3">
+            <div class="row mb-5">
+              <div class="col-lg-6" style="background-color: #e4d9ff;">
+                <div class="card-body rounded-3 p-md-5 mx-md-4" >
+                  <form action="{{ route('register') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                  <div class="text-center mb-5">
+                    <p class="mb-4" style="font-family:Pixel;">Create your account to continue</p>
+                    <div class="avatar-upload">
+                      <div class="avatar-preview" style="background-image: url('https://via.placeholder.com/150');">
+                      </div>
+                      <div class="mt-2">
+                        <label class="btn btn-primary" style="font-family:Pixel; background-color: #273469;
+                        font-size: 15px;">
+                          Upload Image<input type="file" onchange="previewImage(event)" name="default_photo" required>
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  
+                    <div class="row">
+                      <div class="col-md-6 mb-3">
+                        <div class="form-outline">
+                          <input type="text" id="form3Example1m" class="form-control form-control-lg"
+                            placeholder="Juan" name="first_name" required/>
+                          <label class="form-label" for="form3Example1m" style="font-family:Pixel;">First name</label>
+                        </div>
+                      </div>
+                      <div class="col-md-6 mb-3">
+                        <div class="form-outline">
+                          <input type="text" id="form3Example1n" class="form-control form-control-lg"
+                            placeholder="Dela Cruz" name="last_name" required/>
+                          <label class="form-label" for="form3Example1n" style="font-family:Pixel;">Last name</label>
+                        </div>
+                      </div>
+                    </div>
+  
+                    <div class="form-outline mb-3">
+                      <input type="email" id="form2Example11" class="form-control form-control-lg" name="email" required
+                        placeholder="juandelacruz@mail.com" />
+                      <label class="form-label" for="form2Example11" style="font-family:Pixel;">Email Address</label>
+                    </div>
+  
+                    <div class="form-outline mb-3">
+                      <input type="password" id="form2Example22" class="form-control form-control-lg" name="password" required
+                        placeholder="************" />
+                      <label class="form-label" for="form2Example22" style="font-family:Pixel;">Password</label>
+                    </div>
+
+                    <div class="form-outline mb-3">
+                      <input type="password" id="form2Example22" class="form-control form-control-lg" name="password_confirmation" required
+                        placeholder="************" />
+                      <label class="form-label" for="form2Example22" style="font-family:Pixel;">Confirm Password</label>
+                    </div>
+  
+  
+                    <button type="submit" class="btn btn-primary btn-lg btn-block mb-3"
+                      style="font-family:Pixel; width:100%; background-color: #273469;
+                      font-size: 15px;">Sign
+                      in</button>
+                  </form>
+  
+                    <div class="d-flex align-items-center justify-content-center pb-4">
+                      <p class="mb-0 me-2" style="font-family:Pixel; font-size: 13px;">Already have an account?</p>
+                      <a href="login.html" style="font-family:Pixel; font-size: 13px; text-decoration: none;">Login
+                        here.</a>
+                    </div>
+  
+                  
+                </div>
+              </div>
+              <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                  <h4 class="mb-4" style="font-family:Pixel;">Equip your armor, Gamer!</h4>
+                  <p class="small mb-0">Pen & Pixel features a user-friendly interface where gamers can easily create and publish blog posts. You can connect with others and share your passion for gaming. Whether you're a casual gamer or a hardcore enthusiast, you're sure to find something of interest on Pen & Pixel!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 </x-guest-layout>
