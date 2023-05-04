c49e68f5e8f5bcb98827ad9c086084ccbd79bc9f
lolzxc <guitarbeginner2@gmail.com>
2023-05-04 18:03:49 +0800

    ayoko na please

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -101,7 +101,7 @@
                     </div>
                     <div class="row mt-3">
                       <div class="col-1"></div>
-                      <div class="col-2">{{$post->description}}</div>
+                      <div class="col-10">{{$post->description}}</div>
                     </div>
                     <div class="row">
                       <div class="col-1"></div>
